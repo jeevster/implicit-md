fix None gradients error with forcenet energy force gradient calculation

mdsim/common/utils.py
@@ -129,8 +129,8 @@ def compare_gradients(grad1, grad2):
     ratio = (grads1_flattened / (grads2_flattened + 1e-8)).abs().median()
     return cosine_similarity, ratio
 
-def process_gradient(grad, device):
-    return [g.detach() if g is not None else torch.Tensor([0.]).to(device) for g in grad]
+def process_gradient(params, grads, device):
+    return [g.detach() if g is not None else torch.zeros_like(param).to(device) for g, param in zip(grads, params)]
 
 
 def convert_atomic_numbers_to_types(atomic_numbers):

nvt_implicit.py
@@ -320,8 +320,8 @@ class ImplicitMDSimulator():
                 #compute losses
                 energy_loss = self.energy_loss(self.normalizer_train.norm(self.gt_energies_train[start:end]), energy).mean()
                 force_loss = self.force_loss(self.gt_forces_train[start:end].reshape(-1, 3), force.reshape(-1, 3)).mean() 
-                energy_gradients.append(process_gradient(torch.autograd.grad(energy_loss, self.model.parameters(), allow_unused = True, retain_graph = True), self.device))
-                force_gradients.append(process_gradient(torch.autograd.grad(force_loss, self.model.parameters(), allow_unused = True), self.device))
+                energy_gradients.append(process_gradient(self.model.parameters(), torch.autograd.grad(energy_loss, self.model.parameters(), allow_unused = True, retain_graph = True), self.device))
+                force_gradients.append(process_gradient(self.model.parameters(), torch.autograd.grad(force_loss, self.model.parameters(), allow_unused = True), self.device))
             num_params = len(list(self.model.parameters()))
             #energy
             energy_grads_flattened = torch.stack([torch.cat([energy_gradients[j][i].flatten().detach() for i in range(num_params)]) for j in range(num_batches)])
@@ -734,6 +734,8 @@ if __name__ == "__main__":
         #estimate gradients via Fabian method/adjoint
         rdf_package, vacf_package, energy_force_package = \
                     boltzmann_estimator.compute(equilibriated_simulator)
+
+        test = simulator.energy_force_gradient(batch_size=simulator.n_replicas)
                 
         #unpack results
         rdf_grad_batches, mean_rdf, rdf_loss, mean_adf, adf_loss = rdf_package