6dc04d6143c750277a280f6d855c604773d749b9
jeevster <sanjeevr@berkeley.edu>
2023-10-23 11:22:44 -0700

    add job scripts from 10-22 expts

--- run_implicit.sh
+++ run_implicit.sh
@@ -12,19 +12,10 @@
 
 # Define the path to your Python script
 script="/global/homes/s/sanjeevr/implicit-md/nvt_implicit.py"
-config_yml="/global/homes/s/sanjeevr/implicit-md/configs/md22/md22.yml"
-command_inference="python $script --config-yml $config_yml --model=nequip --molecule=$1 --size=10percent --rdf_loss_weight=$2 --vacf_loss_weight=$3 \
-             --energy_loss_weight=$4 --force_loss_weight=$5 --exp_name=$7 --train=False --bond_dev_tol=0.5 --n_epochs=300 --eval_model=$8"
-# command_train="python $script --config-yml $config_yml --model=nequip --molecule=$1 --size=10percent --rdf_loss_weight=$2 --vacf_loss_weight=$3 \
-#                --energy_loss_weight=$4 --force_loss_weight=$5 --exp_name=$7"
-# command_train="python $script --config-yml $config_yml --model=$1 --rdf_loss_weight=$2 --vacf_loss_weight=$3 \
-#              --energy_loss_weight=$4 --force_loss_weight=$5 --reset_probability=$6 --exp_name=$7"
-# command_inference_pre="python $script --config-yml $config_yml --model=nequip --molecule=$1 --size=10percent --rdf_loss_weight=$2 --vacf_loss_weight=$3 --minibatch_size=8 \
-#             --energy_loss_weight=$4 --force_loss_weight=$5 --exp_name=$7 --train=False --bond_dev_tol=0.5 --n_epochs=300 --eval_model=pre"
-# command_inference_post="python $script --config-yml $config_yml --model=nequip --molecule=$1 --size=10percent --rdf_loss_weight=$2 --vacf_loss_weight=$3 --minibatch_size=8 \
-#             --energy_loss_weight=$4 --force_loss_weight=$5 --exp_name=$7 --train=False --bond_dev_tol=0.5 --n_epochs=300 --eval_model=post"
-# srun $command_train
-srun $command_inference
-# srun $command_inference_post
+config_yml="/global/homes/s/sanjeevr/implicit-md/configs/water/train.yml"
+command_train="python $script --config-yml $config_yml --model=$1 --rdf_loss_weight=$2 --vacf_loss_weight=$3 \
+               --energy_force_loss_weight=$4 --only_learn_if_unstable_threshold_reached=$5 --exp_name=$6"
+srun $command_train
+
     
 
--- submit_jobs.sh
+++ submit_jobs.sh
@@ -4,37 +4,21 @@
 # $1: model
 # $2: rdf loss weight
 # $3: vacf loss weight
-# $4: energy loss weight
-# $5: force loss weight
-# $6: reset probability
-# $7: exp_name
+# $4: energy/force loss weight
+# $5: exp_name
 
-molecules_md22=('ac_Ala3_NHMe' 'DHA' 'AT_AT')
-# eval_modes=('post') # 'post' 'lmax=2' 'lmax=3' '50percent' '100percent')
+molecules_md17=('aspirin' 'naphthalene' 'salicylic_acid')
+models=('schnet' 'forcenet')
 # # Iterate through the molecules and run the scripts
-# for mol in "${molecules_md22[@]}"; do
-#     for eval_mode in "${eval_modes[@]}"; do
-#         sbatch run_implicit.sh $mol 1 0 0 0 0.0 'TEST_lmax1_10percent_resumestrategy0.25_learn_continuous_noenergyforce' $eval_mode
-#         sbatch run_implicit.sh $mol 1 0 0.05 0.95 0.0 'TEST_lmax1_10percent_resumestrategy0.25_learn_continuous' $eval_mode
-#         # sbatch run_implicit.sh $mol 1 0 0.10 1.9 0.0 'TEST_lmax1_10percent_resumestrategy0.25_learn_continuous_strongerenergyforce' $eval_mode
-#     done
-# done
-
-eval_modes=('lmax=2' 'lmax=3')
-# Iterate through the molecules and run the scripts
-for mol in "${molecules_md22[@]}"; do
-    for eval_mode in "${eval_modes[@]}"; do
-        # sbatch run_implicit.sh $mol 1 0 0 0 0.0 'TEST_lmax1_10percent_resumestrategy0.25_learn_continuous_noenergyforce' $eval_mode
-        sbatch run_implicit.sh $mol 1 0 0.05 0.95 0.0 'TEST_lmax1_10percent_resumestrategy0.25_learn_continuous' $eval_mode
-        # sbatch run_implicit.sh $mol 1 0 0.10 1.9 0.0 'TEST_lmax1_10percent_resumestrategy0.25_learn_continuous_strongerenergyforce' $eval_mode
-    done
+for model in "${models[@]}"; do
+    sbatch run_implicit.sh $model 1 0 0 True 'TEST_1k_noenergyforce'
+    sbatch run_implicit.sh $model 1 0 1 True 'TEST_1k_energyforce'
+    sbatch run_implicit.sh $model 1 0 2 True 'TEST_1k_strongerenergyforce'
+    sbatch run_implicit.sh $model 1 1 1 True 'TEST_1k_rdf+vacf_energyforce'
+    sbatch run_implicit.sh $model 1 0 0 False 'TEST_1k_noenergyforce_alwayslearn'
+    sbatch run_implicit.sh $model 1 0 1 False 'TEST_1k_energyforce_alwayslearn'
+    sbatch run_implicit.sh $model 1 0 2 False 'TEST_1k_strongerenergyforce_alwayslearn'
+    sbatch run_implicit.sh $model 1 1 1 False 'TEST_1k_rdf+vacf_energyforce_alwayslearn'
 done
 
-# models=('schnet' 'dimenetplusplus')
-# # Iterate through the molecules and run the scripts
-# for model in "${models[@]}"; do
-#     sbatch run_implicit.sh $model 1 0 0 0 0.0 'TEST_resumestrategy0.8_learn_continuous_noenergyforce'
-#     sbatch run_implicit.sh $model 1 0 0.05 0.95 0.0 'TEST_resumestrategy0.8_learn_continuous'
-#     sbatch run_implicit.sh $model 1 0 0.10 1.9 0.0 'TEST_resumestrategy0.8_learn_continuous_strongerenergyforce'
-# done
 
