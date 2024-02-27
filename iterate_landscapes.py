1a211ab3fc245691bdf4f9aba66414356b74f471
jeevster <sanjeevr@berkeley.edu>
2024-02-26 23:27:48 -0800

    remove files

--- iterate_landscapes.py
+++
@@ -1,8 +0,0 @@
-from subprocess import run
-
-distances = [0.1, 0.2, 0.3, 0.4, 0.5]
-
-for dist in distances:
-    command = f"python /home/sanjeevr/implicit-md/ip_explorer/ip_explorer/landscape/__main__.py --num-nodes=1 --save-dir /home/sanjeevr/implicit-md/landscapes --model-type=nequip --database-path=/home/sanjeevr/MDsim/MODELPATH/nequip/md22-ac_Ala3_NHMe_5percent_lmax=1_nequip --model-path=/home/sanjeevr/MDsim/MODELPATH/nequip/md22-ac_Ala3_NHMe_5percent_lmax=1_nequip --landscape-type=plane --no-compute-initial-losses --steps=10 --distance={dist}"
-    run(command.split(" "))
-
--- run_expts_inference.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-from itertools import product
-
-command = f"python nvt_implicit.py --config-yml /home/sanjeevr/implicit-md/configs/water/simulate.yml --eval_model=pre --energy_force_loss_weight=0"
-subprocess.run(command.split(" "))
-for i in range(4):
-    command = f"python nvt_implicit.py --config-yml /home/sanjeevr/implicit-md/configs/water/simulate.yml --eval_model=post_cycle{i+1} --energy_force_loss_weight=0"
-    subprocess.run(command.split(" "))
