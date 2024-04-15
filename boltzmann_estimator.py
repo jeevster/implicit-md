fix rdf vs obs naming convention, remove variance computation

boltzmann_estimator.py
@@ -17,7 +17,6 @@ class BoltzmannEstimator:
     def __init__(
         self,
         gt_rdf_package,
-        gt_rdf_local_package,
         mean_bond_lens,
         gt_vacf,
         gt_adf,
@@ -27,12 +26,11 @@ class BoltzmannEstimator:
         super(BoltzmannEstimator, self).__init__()
         self.params = params
         self.device = device
-        self.diff_rdf = DifferentiableRDF(self.params, device)
-        gt_rdf, gt_rdf_var = gt_rdf_package
-        gt_rdf_local, gt_rdf_var_local = gt_rdf_local_package
 
         # Water system
         if self.params.name == "water":
+            gt_rdf, gt_rdf_local = gt_rdf_package
+            #Training to match RDF
             if params.training_observable == "rdf":
                 self.gt_obs = torch.cat(
                     [rdf.flatten() for rdf in gt_rdf.values()]
@@ -40,35 +38,20 @@ class BoltzmannEstimator:
                 self.gt_obs_local = torch.cat(
                     [rdf.flatten() for rdf in gt_rdf_local.values()]
                 )  # combine RDFs together
-                self.gt_obs_var = torch.cat(
-                    [var.flatten() for var in gt_rdf_var.values()]
-                )  # combine RDF variances together
-                self.gt_obs_var = torch.where(
-                    gt_rdf_var == 0, 1e-2, gt_rdf_var
-                )  # add "hyperprior" to gt variance
-                self.gt_obs_var_local = torch.cat(
-                    [var.flatten() for var in gt_rdf_var_local.values()]
-                )  # combine RDF variances together
-                self.gt_obs_var_local = torch.where(
-                    gt_rdf_var_local == 0, 1e-2, gt_rdf_var_local
-                )  # add "hyperprior" to gt variance
                 self.obs_loss = ObservableMSELoss(self.gt_obs)
             elif params.training_observable == "imd":
+                # Training to match minimum intermolecular distance
                 self.gt_obs = torch.Tensor([1.44]).to(self.device)  # ground truth min IMD
-                self.gt_obs_var_local = torch.Tensor([1.0]).to(self.device)  # identity
                 self.gt_obs_local = self.gt_obs
                 self.obs_loss = IMDHingeLoss(self.gt_obs)
             elif params.training_observable == "bond_length_dev":
                 self.gt_obs = mean_bond_lens[:2].to(self.device)  # ground truth bond length dev
-                self.gt_obs_var_local = torch.Tensor([1.0]).to(self.device)  # identity for now
                 self.gt_obs_local = self.gt_obs
                 self.obs_loss = ObservableMSELoss(self.gt_obs)
 
         else: #MD17 or MD22 system
-            self.gt_obs = gt_rdf
-            self.gt_obs_var = gt_rdf_var
-            self.gt_obs_local = gt_rdf_local
-            self.gt_obs_var_local = gt_rdf_var_local
+            self.gt_obs = gt_rdf_package[0]
+            self.gt_obs_local = self.gt_obs
             self.obs_loss = ObservableMSELoss(self.gt_obs)
 
         #Initialize remaining observables and losses
@@ -78,8 +61,19 @@ class BoltzmannEstimator:
         self.adf_loss = ObservableMSELoss(self.gt_adf)
         self.vacf_loss = ObservableMSELoss(self.gt_vacf)
 
-    # Core Function to compute the gradient estimator
+    
     def estimator(self, g, df_dtheta, mse_gradient):
+        """
+        Computes the Boltzmann gradient estimator described in Eqn. 6 of 
+        the paper. Note: Technically, we should scale the estimator by k_B*T, 
+        but we skip this step as it is essentially like scaling the learning rate
+        Args:
+            g (torch.Tensor): 
+            df_dtheta (torch.Tensor):
+            mse_gradient (torch.Tensor): 
+        """
+
+        # TODO: scale the estimator by Kb*T
         estimator = (
             df_dtheta.mean(0).unsqueeze(0) * g.mean(0).unsqueeze(-1)
             - df_dtheta.unsqueeze(1) * g.unsqueeze(-1)
@@ -90,8 +84,11 @@ class BoltzmannEstimator:
         )[0]
         return estimator.detach()
 
+
     def compute(self, simulator):
         self.simulator = simulator
+
+        #initialize observable computation functions
         diff_rdf = DifferentiableRDF(self.params, self.device)
         diff_adf = DifferentiableADF(
             self.simulator.n_atoms,
@@ -183,16 +180,14 @@ class BoltzmannEstimator:
         else:
             energy_force_package = None
 
-        # VACF stuff - always none - TODO: remove
-        vacf_gradient_estimators = None
+        # VACF stuff
         vacf_package = (
-            vacf_gradient_estimators,
             vacfs_per_replica,
             mean_vacf_loss.to(self.device),
         )
 
         ###Main Observable Stuff ###
-        if self.simulator.pbc:  # LiPS or Water
+        if self.simulator.pbc:  # Water
             if self.simulator.training_observable == "imd":
                 obs = torch.cat(
                     [self.simulator.min_imd(s.unsqueeze(0)) for s in stacked_radii])
@@ -237,7 +232,7 @@ class BoltzmannEstimator:
 
         #If we aren't in the learning phase, return without computing the Boltzmann estimator
         if (
-            self.params.rdf_loss_weight == 0
+            self.params.obs_loss_weight == 0
             or not self.simulator.train
             or simulator.optimizer.param_groups[0]["lr"] == 0
         ):
@@ -263,6 +258,7 @@ class BoltzmannEstimator:
                 local_stacked_radii, 
                 atomic_indices, 
                 bond_lens,
+                imds,
             ) = self.process_local_neighborhoods(stacked_radii)
 
         else:
@@ -272,7 +268,6 @@ class BoltzmannEstimator:
         obs.requires_grad = True
         adfs.requires_grad = True
 
-        # TODO: scale the estimator by Kb*T
         # shuffle the radii, rdfs, and losses
         if self.simulator.shuffle:
             shuffle_idx = torch.randperm(stacked_radii.shape[0])
@@ -379,11 +374,10 @@ class BoltzmannEstimator:
             obs_batch = [obs[start:end]]
             pe_grads_batch = pe_grads_flattened[start:end]
             grad_outputs_obs = [
-                2 / gt_obs_var_local * (ob.mean(0) - gt_ob).unsqueeze(0)
-                for ob, gt_ob, gt_obs_var_local in zip(
+                2  * (ob.mean(0) - gt_ob).unsqueeze(0)
+                for ob, gt_ob in zip(
                     obs_batch,
                     self.gt_obs_local.chunk(len(obs_batch)),
-                    self.gt_obs_var_local.chunk(len(obs_batch)),
                 )
             ]
             final_vjp = [
@@ -393,18 +387,21 @@ class BoltzmannEstimator:
             final_vjp = torch.stack(final_vjp).mean(0)
 
             raw_grads.append(final_vjp)
-            mean_vjps = torch.stack(raw_grads).mean(dim=0)
-            # re-assemble flattened gradients into correct shape
-            mean_vjps = tuple(
-                [
-                    g.reshape(shape)
-                    for g, shape in zip(
-                        mean_vjps.split(original_numel), original_shapes
-                    )
-                ]
-            )
-            obs_gradient_estimators.append(mean_vjps)
+        
+        # compute mean across minibatches
+        mean_vjps = torch.stack(raw_grads).mean(dim=0)
+        # re-assemble flattened gradients into correct shape
+        mean_vjps = tuple(
+            [
+                g.reshape(shape)
+                for g, shape in zip(
+                    mean_vjps.split(original_numel), original_shapes
+                )
+            ]
+        )
+        obs_gradient_estimators.append(mean_vjps)
 
+        #return final quantities
         obs_package = (
             obs_gradient_estimators,
             mean_obs,
@@ -414,6 +411,7 @@ class BoltzmannEstimator:
         )
 
         return obs_package, vacf_package, energy_force_package
+
 
 
     def process_local_neighborhoods(self, stacked_radii):
@@ -521,6 +519,7 @@ class BoltzmannEstimator:
         )
         bond_lens = full_list
 
+        #Save relevant quantities
         np.save(
             os.path.join(
                 self.simulator.save_dir,
@@ -544,5 +543,5 @@ class BoltzmannEstimator:
             bond_len_devs.cpu(),
         )
 
-        return stacked_radii, local_stacked_radii, atomic_indices, bond_lens
+        return stacked_radii, local_stacked_radii, atomic_indices, bond_lens, imds
 

mdsim/observables/water.py
@@ -33,7 +33,7 @@ class WaterRDFMAE(torch.nn.Module):
         for i in range(
             stacked_radii.shape[1]
         ):  # explicit loop since vmap makes some numpy things weird
-            rdfs, _ = get_water_rdfs(
+            rdfs = get_water_rdfs(
                 stacked_radii[:, i],
                 self.ptypes[: stacked_radii.shape[-2]],
                 self.lattices,
@@ -168,7 +168,6 @@ def get_water_rdfs(data_seq, ptypes, lattices, bins, device='cpu'):
     
     data_seq = ((data_seq / lattices) % 1) * lattices #coords are wrapped
     all_rdfs = {}
-    all_rdfs_vars = {}
     n_rdfs = 3
     for idx in range(n_rdfs):
         type1, type2 = pairs[idx]    
@@ -194,10 +193,8 @@ def get_water_rdfs(data_seq, ptypes, lattices, bins, device='cpu'):
         data_rdfs = data_hists / Z_data
         data_rdfs = data_rdfs / data_rdfs.sum(1, keepdims=True) * data_rdf.sum()#normalize to match original sum
         data_rdf_mean = data_rdfs.mean(0)
-        data_rdf_var = data_rdfs.var(0)
         all_rdfs[type1 + type2] = torch.Tensor([data_rdf_mean]).to(device)
-        all_rdfs_vars[type1 + type2] = torch.Tensor([data_rdf_var]).to(device)
-    return all_rdfs, all_rdfs_vars
+    return all_rdfs
 
 
 def find_water_rdfs_diffusivity_from_file(base_path: str, size: str, params, device):

stable_training.py
@@ -206,10 +206,11 @@ if __name__ == "__main__":
     ###########Compute ground truth observables###############
 
     print("Computing ground truth observables from datasets")
+
     if name == "water":
         (
-            gt_rdf_package,
-            gt_rdf_local_package,
+            gt_rdf,
+            gt_rdf_local,
             gt_diffusivity,
             gt_msd,
             gt_adf,
@@ -217,56 +218,37 @@ if __name__ == "__main__":
         ) = find_water_rdfs_diffusivity_from_file(
             data_path, MAX_SIZES[name], params, device
         )
-        gt_rdf, gt_rdf_var = gt_rdf_package
-        gt_rdf_local, gt_rdf_var_local = gt_rdf_local_package
-    elif name == "lips":
-        gt_rdf, gt_diffusivity = find_lips_rdfs_diffusivity_from_file(
-            data_path, MAX_SIZES[name], params, device
-        )
-        gt_rdf_var = torch.ones_like(gt_rdf)
-        gt_adf = torch.zeros((100, 1)).to(device)  # TODO: temporary
-
+        gt_rdf_package = (gt_rdf, gt_rdf_local)
+        
     else:
         gt_rdf, gt_adf = find_hr_adf_from_file(
             data_path, name, molecule, MAX_SIZES[name], params, device
         )
-        gt_rdf_var = torch.ones_like(gt_rdf)
-        gt_rdf_package = (gt_rdf, gt_rdf_var)
-        gt_rdf_local_package = (gt_rdf, gt_rdf_var)
+        gt_rdf_package = (gt_rdf,)
+
     contiguous_path = os.path.join(
         data_path, f"contiguous-{name}", molecule, MAX_SIZES[name], "val/nequip_npz.npz"
     )
     gt_data = np.load(contiguous_path)
     if name == "water":
         gt_vels = torch.FloatTensor(gt_data.f.velocities).to(device)
-    elif name == "lips":
-        gt_traj = torch.FloatTensor(gt_data.f.pos).to(device)
-        gt_vels = gt_traj[1:] - gt_traj[:-1]  # finite difference approx
     else:
         gt_traj = torch.FloatTensor(gt_data.f.R).to(device)
-        gt_vels = gt_traj[1:] - gt_traj[:-1]  # finite difference approx
+        gt_vels = gt_traj[1:] - gt_traj[:-1]  # finite difference approximation
 
     gt_vacf = DifferentiableVACF(params, device)(gt_vels)
-    if isinstance(gt_rdf, dict):
+    if name == 'water':
         for _type, _rdf in gt_rdf.items():
             np.save(os.path.join(results_dir, f"gt_{_type}_rdf.npy"), _rdf[0].cpu())
             np.save(
-                os.path.join(results_dir, f"gt_{_type}_rdf_var.npy"),
-                gt_rdf_var[_type].cpu(),
-            )
-            np.save(
                 os.path.join(results_dir, f"gt_{_type}_rdf_local.npy"),
                 gt_rdf_local[_type][0].cpu(),
-            )
-            np.save(
-                os.path.join(results_dir, f"gt_{_type}_rdf_var_local.npy"),
-                gt_rdf_var_local[_type].cpu(),
             )
     else:
         np.save(os.path.join(results_dir, "gt_rdf.npy"), gt_rdf.cpu())
     np.save(os.path.join(results_dir, "gt_adf.npy"), gt_adf.cpu())
     np.save(os.path.join(results_dir, "gt_vacf.npy"), gt_vacf.cpu())
-    if params.name == "lips" or params.name == "water":
+    if name == "water":
         np.save(os.path.join(results_dir, "gt_diffusivity.npy"), gt_diffusivity.cpu())
         np.save(os.path.join(results_dir, "gt_msd.npy"), gt_msd.cpu())
 
@@ -276,7 +258,7 @@ if __name__ == "__main__":
         5**params.max_times_reduce_lr
     )  # LR reduction factor is 0.2 each time
     losses = []
-    rdf_losses = []
+    obs_losses = []
     adf_losses = []
     diffusion_losses = []
     all_vacfs_per_replica = []
@@ -297,18 +279,11 @@ if __name__ == "__main__":
     changed_lr = False
     cycle = 0
     learning_epochs_in_cycle = 0
-    # function to add gradients
-    add_lists = lambda list1, list2, w1, w2: tuple(
-        [w1 * l1 + w2 * l2 for l1, l2 in zip(list1, list2)]
-    )
-
+    
     ######### Begin StABlE Training Loop #############
 
     for epoch in range(params.n_epochs):
-        # rdf = torch.zeros_like(gt_rdf).to(device)
-        rdf_loss = torch.Tensor([0]).to(device)
-        vacf_loss = torch.Tensor([0]).to(device)
-        diffusion_loss = torch.Tensor([0]).to(device)
+        
         best = False
         grad_cosine_similarity = 0
         ratios = 0
@@ -322,7 +297,6 @@ if __name__ == "__main__":
             # initialize Boltzmann_estimator
             boltzmann_estimator = BoltzmannEstimator(
                 gt_rdf_package,
-                gt_rdf_local_package,
                 simulator.mean_bond_lens,
                 gt_vacf,
                 gt_adf,
@@ -347,6 +321,7 @@ if __name__ == "__main__":
             )
             print(f"Initialize {simulator.n_replicas} random ICs in parallel")
 
+        
         simulator.optimizer.zero_grad()
         simulator.epoch = epoch
 
@@ -369,32 +344,33 @@ if __name__ == "__main__":
         # If instability threshold reached, estimate gradients via Boltzmann estimator
         # Otherwise, the gradients are None
         (
-            rdf_package,
+            obs_package,
             vacf_package,
             energy_force_grad_batches,
         ) = boltzmann_estimator.compute(equilibriated_simulator)
 
         # unpack results
-        rdf_grad_batches, mean_rdf, rdf_loss, mean_adf, adf_loss = rdf_package
-        _, vacfs_per_replica, vacf_loss = vacf_package
+        obs_grad_batches, mean_obs, obs_loss, mean_adf, adf_loss = obs_package
+        vacfs_per_replica, vacf_loss = vacf_package
         if not params.train:
             all_vacfs_per_replica.append(vacfs_per_replica)
 
         # Compute observable loss
-        outer_loss = params.rdf_loss_weight * rdf_loss
-        print(f"Loss: RDF={rdf_loss.item()}")
+        outer_loss = params.obs_loss_weight * obs_loss
+        print(f"Observable Loss ={outer_loss.item()}")
 
         # make lengths match for iteration
         if energy_force_grad_batches is None:
-            energy_force_grad_batches = rdf_grad_batches
+            energy_force_grad_batches = obs_grad_batches
 
         # Learning phase gradient update
-        if rdf_grad_batches is not None:
+        if obs_grad_batches is not None:
             grad_cosine_similarity = []
             ratios = []
-            for rdf_grads, energy_force_grads in zip(
-                rdf_grad_batches, energy_force_grad_batches
-            ):  # loop through minibatches
+            for obs_grads, energy_force_grads in zip(
+                obs_grad_batches, energy_force_grad_batches
+            ):  # loop through gradients - should only be a single step 
+                # since we don't allow off-policy gradients
                 simulator.optimizer.zero_grad()
 
                 # modify gradients according to loss weights
@@ -404,7 +380,7 @@ if __name__ == "__main__":
                         for ef_grad in energy_force_grads
                     ]
                 )
-                cosine_similarity, ratio = compare_gradients(rdf_grads, ef_grads)
+                cosine_similarity, ratio = compare_gradients(obs_grads, ef_grads)
                 grad_cosine_similarity.append(
                     cosine_similarity
                 )  # compute gradient similarities
@@ -412,7 +388,7 @@ if __name__ == "__main__":
 
                 # Loop through each group of parameters and set gradients
                 for param, obs_grad, ef_grad in zip(
-                    model.parameters(), rdf_grads, ef_grads
+                    model.parameters(), obs_grads, ef_grads
                 ):
                     param.grad = obs_grad + ef_grad
 
@@ -487,7 +463,7 @@ if __name__ == "__main__":
 
         # log stats
         losses.append(outer_loss.item())
-        rdf_losses.append(rdf_loss.item())
+        obs_losses.append(obs_loss.item())
         adf_losses.append(adf_loss.item())
         vacf_losses.append(vacf_loss.item())
         mean_instabilities.append(equilibriated_simulator.mean_instability)
@@ -516,7 +492,7 @@ if __name__ == "__main__":
             # reached instability point, can stop
             break
         writer.add_scalar("Loss", losses[-1], global_step=epoch + 1)
-        writer.add_scalar("RDF Loss", rdf_losses[-1], global_step=epoch + 1)
+        writer.add_scalar("Observable Loss", obs_losses[-1], global_step=epoch + 1)
         writer.add_scalar("ADF Loss", adf_losses[-1], global_step=epoch + 1)
         writer.add_scalar("VACF Loss", vacf_losses[-1], global_step=epoch + 1)
         if params.stability_criterion == "imd":